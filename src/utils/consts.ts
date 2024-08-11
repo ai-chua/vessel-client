d4d3d8f73def6a464c1b51fdf54e482b071f9f29
Ailin Chua <ai-chua@outlook.com>
2024-08-11 11:25:39 +0800

    added time consts

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,1 +1,4 @@
 export const SERVER_PORT = process.env.NEXT_PUBLIC_SERVER_PORT ? parseFloat(process.env.NEXT_PUBLIC_SERVER_PORT) : 4000
+
+export const SECOND_IN_MS = 1000
+export const MIN_IN_MS = SECOND_IN_MS * 60
