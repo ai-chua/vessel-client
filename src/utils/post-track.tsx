924a994468ea1a94bdb12a53909c54a59f25eac5
Ailin Chua <ai-chua@outlook.com>
2024-08-11 11:26:01 +0800

    post track req fn

---
+++ src/utils/post-track.tsx
@@ -0,0 +1,22 @@
+import { SERVER_PORT } from './consts'
+import { VesselInformation } from './types'
+
+export async function postTrackRequest(vesselIds: VesselInformation['imo'][]): Promise<void> {
+  try {
+    const response = await fetch(`http://localhost:${SERVER_PORT}/api/track`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(vesselIds)
+    })
+
+    if (response.status !== 200) {
+      alert(`Failed to track vessels: ${response.status}`)
+      return
+    }
+    console.info('Success making POST /track call')
+  } catch (error) {
+    console.error('Error tracking vessels:', error)
+  }
+}
